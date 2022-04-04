0770de5d87cf0b6a7ab945cc6e4cb416f570e2ee
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-03 23:08:44 -0300

    Pre-programming configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 // Importação das tipagens do 'Component'
 import { AppProps } from 'next/app'
 
+import '../styles/global.scss'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
