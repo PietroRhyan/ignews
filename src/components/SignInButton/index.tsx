c1739f92f7a75c36c61bfff70469b9ac542b4431
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-08 18:33:01 -0300

    Adding OAuth with GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,17 +3,25 @@ import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
-export function SignInButton() {
-  const isUserLoggedIn = true
+import { signIn, signOut, useSession } from 'next-auth/react'
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+export function SignInButton() {
+  const {data: session} = useSession()
+
+  return session ? (
+    <button 
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04D361'/>
-      Pietro Rhyan
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button 
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color='#EBA417'/>
       Sign in with Github
     </button>
