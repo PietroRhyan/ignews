6290e5e9557a81f669f108dbaf59f53a42ecce0f
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-16 00:21:22 -0300

    Adding Prismic CMS funcionalities

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,53 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic'
+import styles from './styles.module.scss'
+
+import Prismic from '@prismicio/client'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Criando um Monorepo com Lerna e Yarn Workspaces</strong>
+            <p>Neste guia, você irá aprender como criar um Monorepo para gerenciar múltiplos pacotes com compartilhamento simultâneo</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Criando um Monorepo com Lerna e Yarn Workspaces</strong>
+            <p>Neste guia, você irá aprender como criar um Monorepo para gerenciar múltiplos pacotes com compartilhamento simultâneo</p>
+          </a>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Criando um Monorepo com Lerna e Yarn Workspaces</strong>
+            <p>Neste guia, você irá aprender como criar um Monorepo para gerenciar múltiplos pacotes com compartilhamento simultâneo</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  console.log(response)
+
+  return {
+    props: {}
+  }
+} 
