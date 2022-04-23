6e8b4eaa7a15f154ab29f682fd9affc6af5269e4
Pietro Rhyan <pietroqjg@gmail.com>
2022-04-23 12:21:52 -0300

    Finalizing the project

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,8 +39,15 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req })
   const { slug } = params
-  // if (!session) {   
-  // }
+  
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: `/posts/preview/${slug}`,
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
